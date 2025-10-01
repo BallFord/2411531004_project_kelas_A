9dc933ac8e1e83b59673fbab6a35236ad97f5737
Lenovo <Lenovo@LAPTOP-2P047SBL>
2025-10-01 09:51:00 +0700

    Praktikum Pekan 4

---
+++ SistemManajemenPerpustakaan/src/DefaultPackage/Textbook.java
@@ -0,0 +1,12 @@
+package DefaultPackage;
+
+public class Textbook extends Book {
+    private String subject;
+
+    public Textbook(String title, String author, String subject) {
+        super(title, author);
+        this.subject = subject;
+    }
+
+    public String getSubject() { return subject; }
+}
